a1b178bccb696e039ab9c4250ef99f632135afaf
zahin00x <zahin0013@gmail.com>
2021-12-29 16:53:52 +0600

    UI components are imported into admin new-post page

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -7,14 +7,21 @@
                 <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
                 <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
                 <AppButton type="submit">Save</AppButton>
-                <AppButton  type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
+                <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
             </form>
         </section>
     </div>
 </template>
 
 <script> 
+import AppControlInput from '@/components/UI/AppControlInput.vue'
+import AppButton from '@/components/UI/AppButton.vue'
+
     export default {
+        components: {
+            AppControlInput,
+            AppButton
+        },
         data()
         {
             return {
@@ -30,10 +37,12 @@
             onSave()
             {
                 //saveThePost
+                console.log(this.editedPost);
             },
             onCancel()
             {
                 //NavigateBack
+                this.$router.push('/admin');
             }
         }
 
