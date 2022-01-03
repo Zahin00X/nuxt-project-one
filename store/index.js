b34cf9d5b89887b4987355d36657f8ed23659046
zahin00x <zahin0013@gmail.com>
2022-01-03 16:39:12 +0600

    vuex store added

---
+++ store/index.js
@@ -0,0 +1,28 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+    return new Vuex.Store({
+        state:{
+            loadedPosts : []
+        },
+        mutations:{
+            setPost(state, post)
+            {
+                state.loadedPosts = post;
+            }
+        },
+        actions: {
+            setPost(vuexContext, post)
+            {
+                vuexContext.commit("setPost", post);
+            }
+        },
+        getters:{
+            loadedPosts(state)
+            {
+                return state.loadedPosts
+            }
+
+        }
+    })
+}
