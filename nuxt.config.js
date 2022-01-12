7cca654ed95ed180433b5534ecbfe4ee4f2759cc
zahin00x <zahin0013@gmail.com>
2022-01-12 15:00:58 +0600

    nuxt.config.js file updated, title of the app and single posts are modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,26 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-project-one',
+    title: 'WD Blog',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'My cool web development blog' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
            { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap"}],
+  },
+
+  loading: { 
+    color: 'FF0000',
+    height: '10px',
+    duration: 5000,
+    failedColor: 'yellow',
+    throttle: 0
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
